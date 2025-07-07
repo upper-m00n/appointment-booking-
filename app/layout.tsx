4e958daba707001eb3d396b5d121f96f645e2043
Ashutosh Sharma <mr.proashutosh@gmail.com>
2025-07-08 03:49:13 +0530

    fixed styliing

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,14 @@
 import "./globals.css";
 import Sidebar from "@/components/Sidebar";
+import {Open_Sans} from 'next/font/google'
+
+
+const openSans = Open_Sans({
+  subsets: ['latin'],
+  weight: ['400', '600', '700'],
+  variable: '--font-open-sans',
+  display: 'swap',
+});
 
 export const metadata = {
   title: "Appointment Booking",
@@ -12,8 +21,8 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html lang="en">
-      <body className="flex">
+    <html lang="en" className={openSans.variable}>
+      <body className="flex font-sans bg-gray-50">
         <Sidebar />
         <main className="flex-1 bg-gray-50 min-h-screen p-6">{children}</main>
       </body>
